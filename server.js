969814a87dcdbde3a0adcaef734cbe211c779066
nouri <60990203+nouri89@users.noreply.github.com>
2020-10-31 14:24:42 +0000

    .gitignore

--- server.js
+++ server.js
@@ -13,11 +13,53 @@ const quotes = require("./quotes.json");
 //   /quotes            - Should return all quotes (json)
 //   /quotes/random     - Should return ONE quote (json)
 app.get("/", function (request, response) {
-  response.send("Neill's Quote Server!  Ask me for /quotes/random, or /quotes");
+	response.send(
+		"Nouri's Quote Server!  Ask me for /quotes/random, or /quotes  or /search?author or/search?term"
+	);
 });
 
 //START OF YOUR CODE...
+app.get("/quotes", function (request, response) {
+	response.send(quotes);
+});
 
+app.get("/quotes/random", function (request, response) {
+	response.send(pickFromArray(quotes));
+});
+
+app.get("/search", function (req, res) {
+	let searchedWord = req.query;
+	if (Object.keys(searchedWord)[0] === "author") {
+		let searchedAuthor = req.query.author;
+
+		const searchResults = quotes.filter((q) => {
+			return q.author.toLowerCase().includes(searchedAuthor.toLowerCase());
+		});
+
+		console.log(searchResults.length);
+		console.log(quotes.length);
+
+		if (searchResults.length > 0) {
+			res.send(searchResults);
+		} else {
+			res.end(`search was not found!`);
+		}
+	} else if (Object.keys(searchedWord)[0] === "term") {
+		let searchedWord = req.query.term;
+		const searchResults = quotes.filter((q) => {
+			return q.quote.toLowerCase().includes(searchedWord.toLowerCase());
+		});
+
+		console.log(searchResults.length);
+		console.log(quotes.length);
+
+		if (searchResults.length > 0) {
+			res.send(searchResults);
+		} else {
+			res.end(`search was not found!`);
+		}
+	}
+});
 //...END OF YOUR CODE
 
 //You can use this function to pick one element at random from a given array
@@ -25,10 +67,15 @@ app.get("/", function (request, response) {
 //example: pickFromArray(myContactsArray)
 //
 function pickFromArray(arr) {
-  return arr[Math.floor(Math.random() * arr.length)];
+	return arr[Math.floor(Math.random() * arr.length)];
 }
 
-//Start our server so that it listens for HTTP requests!
-const listener = app.listen(process.env.PORT, function () {
-  console.log("Your app is listening on port " + listener.address().port);
+app.get("/json", (req, res) => {
+	res.json({ message: "Hello" });
 });
+
+//Start our server so that it listens for HTTP requests!
+const listener = app.listen(3000, function () {
+	console.log("Your app is listening on port 3000");
+});
+//process.env.PORT
